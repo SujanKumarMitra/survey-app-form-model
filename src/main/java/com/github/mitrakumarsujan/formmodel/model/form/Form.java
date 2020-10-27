e88572e591d9f5b31a6e30b26fa6b4252e9c5d57
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-10-27 20:20:12 +0530

    added json serialize as annotation

--- src/main/java/com/github/mitrakumarsujan/formmodel/model/form/Form.java
+++ src/main/java/com/github/mitrakumarsujan/formmodel/model/form/Form.java
@@ -3,12 +3,14 @@ package com.github.mitrakumarsujan.formmodel.model.form;
 import java.io.Serializable;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 /**
  * @author Sujan Kumar Mitra
  * @since 2020-10-25
  */
 @JsonPropertyOrder({ "uid", "key", "template" })
+@JsonDeserialize(as = MutableForm.class)
 public interface Form extends Serializable {
 
 	String getUID();
