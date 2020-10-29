0d1ed59666c72ea6187c755c240f377c4fcfc06a
Sujan Kumar Mitra <mitrakumarsujan@gmail.com>
2020-10-29 19:33:53 +0530

    added utility functions for Collectors

---
+++ src/main/java/com/github/mitrakumarsujan/formmodel/model/util/CollectorUtils.java
@@ -0,0 +1,23 @@
+package com.github.mitrakumarsujan.formmodel.model.util;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Sujan Kumar Mitra
+ * @since 2020-10-29
+ */
+@Component
+public class CollectorUtils {
+
+	public <K, V> Map<K, V> toIdentityMap(Collection<? extends V> collection,
+			Function<? super V, ? extends K> keyMapper) {
+
+		return collection	.parallelStream()
+							.collect(Collectors.toUnmodifiableMap(keyMapper, Function.identity()));
+	}
+}
